4c2bd1945bfd550003d9ae8af3469ba06117008c
Elena Deras <mariad3r4s@gmail.com>
2025-12-02 08:18:30 -0600

    [ADD]: all

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import Copyright from "../Copyright/Copyrighy";
+import "./Footer.css";
 
 function Footer() {
     return ( 
         <>
             <footer className="footer site__section">
                 <p className="footer__text">© Magical Mexico</p>
+                <p className="footer__text">María Elena Deras Barrientos UTLD</p>
             </footer>
         </>
      );
