bd8fc60e483f8d68b10dc2707ea87ae6aace12dc
Elena Deras <mariad3r4s@gmail.com>
2025-12-03 16:30:39 -0600

    [FIX]: Like

--- src/Componentes/Main/Profile/Profile.jsx
+++ src/Componentes/Main/Profile/Profile.jsx
@@ -4,17 +4,17 @@ function Profile() {
     return ( 
         <>
             <div className="profile site__section">
-                <img className="profile__image" src="/assets/images/avatar-perfil.png" alt="Avatar" />
+                <img className="profile__image" src="/assets/images/profile.jpg" alt="Avatar" />
                 
                 <div className="profile__details">
-                    <h1 className="profile__name" id="h1temporal">El diavlo</h1>
+                    <h1 className="profile__name" id="h1temporal">CardCaptor Sakura</h1>
                     <button
                     aria-label="Edit traveler profile"
                     className="profile__edit-btn"
                     type="button"
                     id="button-edit"
                     ></button>
-                    <p className="profile__bio">Ando suelto, pa 🔥🔥</p>
+                    <p className="profile__bio">Clow cards 🌸🌸</p>
                 </div>
                 <button aria-label="Add new place" className="profile__add-place-btn" type="button"></button>
             </div>
